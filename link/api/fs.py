6b31f8148a8779718052d8561943b01248d8075f
unknown <kindermolina98@gmail.com>
2025-08-27 16:10:10 -0300

    changing to FILE object;

--- link/api/fs.py
+++ link/api/fs.py
@@ -1,14 +1,195 @@
-import pandas as pd
 import segysak
-import xarray as xr
 import filetype
 import base64
+import tarfile
+import segyio
+
+import pandas as pd
+import xarray as xr
+
 from io import BytesIO
 
-import segyio
 from link.api.grapher import do2dGraph
 from link.api.data_analysis import PCAnalysis, ICAnalysis, Basics
 from segprocess.signal_processing import FFilter
+
+class File:
+    def __init__(self, file, data):
+        self.file   = file
+        self.segy   = False
+        self.csv    = False
+        self.tsv    = False
+        self.name   = None
+        self.sep    = None
+
+        self.segy_params = ['nmo', 'gain', 'fs', 'fc', 'filtype', 'filtname', 
+                            'window', 'numtaps', 'order', 'convolve']
+        self.stat_params = ['pca', 'ica', 'complete', 'basics', 'target', 'ncomps']
+
+        self.params = {}
+        self.report = {}
+        
+        self.file_format = data.get()
+        self.basics = data.get('basics')
+        self.complete = data.get('complete')
+        self.pca = data.get('pca')
+        self.ica = data.get('ica')
+        self.nothing = False
+
+        self.ffilter    = data.get('ffilter')
+        self.nmo        = data.get('nmo')
+        self.convolve   = data.get('convolve')
+
+        self.gain       = data.get('gain')
+        self.fs         = data.get('fs')
+        self.cut_freq   = data.get('fc')
+        self.filtertype = data.get('filtype')
+        self.filtername = data.get('filtname')
+        self.window     = data.get('window')
+        self.numtaps    = data.get('numtaps')
+        self.order      = data.get('order')
+
+        self.xdata = data.get('xdata')
+        self.ydata = data.get('ydata')
+
+        self.target = data.get('target')
+        self.ncomps = data.get('ncomps')
+
+    def set_params(self, which):
+        if which == 'stat':
+            self.params['complete'] = self.complete
+            self.params['basics'] = self.basics
+            self.params['pca'] = self.pca
+            self.params['ica'] = self.ica
+            self.params['target'] = self.target
+            self.params['ncomps'] = self.ncomps
+        elif which== 'segy':
+            self.params['order']    = self.order
+            self.params['numtaps']  = self.numtaps
+            self.params['convolve'] = self.convolve
+            self.params['ffilter']  = self.ffilter
+            self.params['nmo']      = self.nmo
+            self.params['cut_freq'] = self.cut_freq
+            self.params['window']   = self.window
+            self.params['fs']   = self.fs
+            self.params['gain'] = self.gain
+            self.params['filtertype']   = self.filtertype
+            self.params['filtername']   = self.filtername
+        elif which == 'zip':
+            print('idk')
+        else:
+            print('not found file type')
+
+    def set_sep(self):
+        if self.csv == True:
+            self.sep = ","
+        elif self.tsv == True:
+            self.sep = "\t"
+        elif self.ssv == True:
+            self.sep = " "
+        else:
+            print('not separator')
+
+
+    def file_type(self, form):
+        kind = filetype.guess(self.file)
+        myformat = ''
+        if kind is None:
+            self.segy   = issegy(self.file)
+            self.csv    = is_sv(self.file, ',')
+            self.tsv    = is_sv(self.file, '\t')
+            self.ssv    = is_sv(self.file, ' ')
+        else:
+            self.csv = False
+            self.tsv = False
+            self.segy = False
+            self.ssv = False
+
+    def read_file(self):
+        #take_extension()
+        self.file_type()
+        self.set_sep()
+        if (self.csv == True or self.tsv == True or self.ssv == True):
+            self.set_params('stat')
+            res, report = self.stat_reader()
+            self.report = report
+        elif (self.segy == True):
+            self.set_params('segy')
+            res, report = self.segy_reader()
+            self.report = report
+        else:
+            self.params = {}
+            self.report = {}
+
+    def stat_reader(self):
+        bin_fl_object   = BytesIO(self.file) #binary file-like object
+        data            = pd.read_csv(bin_fl_object, sep=self.sep, encoding='latin1')
+        report = {}
+        if self.complete == True:
+            res, image = do2dGraph(data, self.params)
+            report['2dgraph'] = image
+            res, image = PCAnalysis(data, self.params)
+            report['pca_report'] = image
+            res, image = ICAnalysis(data, self.params)
+            report['ica_report'] = image
+            res, image = Basics(data, self.params)
+            report['basics_report'] = image
+            self.report = report
+        elif self.nothing == True:
+            res, cnt = do2dGraph(data, self.params)
+            report['2dgraph'] = cnt
+            self.report = report
+        elif self.basics == True:
+            res, basics_report = Basics(data, self.params)
+            report['basics_report'] = basics_report
+            res, image = do2dGraph(data, self.params)
+            report['2dgraph'] = image
+            self.report = report
+        elif self.pca == True:
+            res, cnt = do2dGraph(data, self.params)
+            report['2dgraph'] = cnt
+            res, cnt = PCAnalysis(data, self.params)
+            report['pca_report'] = cnt
+            self.report = report
+        elif self.ica == True:
+            res, cnt = do2dGraph(data, self.params)
+            report['2dgraph'] = cnt
+            res, cnt = ICAnalysis(data, self.params)
+            report['ica_report'] = cnt
+            self.report = report
+        else:
+            self.report = {}
+
+    def segy_reader(self.file):
+        if params['process']:
+            if self.nmo == True:
+                res, report = NMOfilter(self.file, self.params)
+                self.report = report
+            elif self.ffilter == True:
+                res, report = FFilter(self.file, self.params)
+                self.report = report
+            else:
+                print('nothing to process')
+                self.report = report
+
+    def dat_reader(self):
+        pass
+    def zip_reader(self):
+        #unzip(file_str)
+        #return True 
+        pass    
+    
+    def tar_reader(self):
+        with tarfile.open(fileobject, 'r:*') as tar_ref:
+            tar_ref.extractall(extract_to)
+
+def isvalid_base64(s):
+    try:
+        base64.b64decode(s, validate=True)
+        return True
+    except Exception:
+        return False
+
 
 def issegy(bin_data):
     try:
@@ -19,149 +200,12 @@ def issegy(bin_data):
     except Exception as e:
         return False
 
-def iscsv(bin_data):
+def is_sv(bin_data, sep):
     text = bin_data.decode("utf-8", errors="ignore")
     lines = text.splitlines()
-    sep = ","
     ncols = len(lines[0].split(sep))
     for line in lines:
         col = len(line.split(sep))
         if col != ncols:
             return False
     return True
-
-def FileType(file_str, form):
-    bin_data = base64.b64decode(file_str)
-    kind = filetype.guess(bin_data)
-    myformat = ''
-    if kind is None:
-        res_seg = issegy(bin_data)
-        if res_seg:
-            if (form == 'segy'):
-                return True
-        res_csv = iscsv(bin_data)
-        if res_csv:
-            if (form == 'csv'):
-                return True
-        return False
-    else:
-        if(kind.extension == form):
-            return True
-        else:
-            return False
-
-def CSVreader(file_str, params):
-    bytes_file      = base64.b64decode(file_str)
-    bin_fl_object   = BytesIO(bytes_file) #binary file-like object
-    data            = pd.read_csv(bin_fl_object, sep=',', encoding='latin1')
-    report = {}
-    if params['process'] == 'complete':
-        res, image = do2dGraph(data, params)
-        report['2dgraph'] = image
-        res, image = PCAnalysis(data, params)
-        report['pca_report'] = image
-        res, image = ICAnalysis(data, params)
-        report['ica_report'] = image
-        res, image = Basics(data, params)
-        report['basics_report'] = image
-        return True, report
-    elif params['process'] == 'nothing':
-        res, cnt = do2dGraph(data, params)
-        report['2dgraph'] = cnt
-        return True, report
-    elif params['process'] == 'basics':
-        res, basics_report = Basics(data, params)
-        report['basics_report'] = basics_report
-        res, image = do2dGraph(data, params)
-        report['2dgraph'] = image
-        return True, report
-    elif params['process'] == 'pca':
-        res, cnt = do2dGraph(data, params)
-        report['2dgraph'] = cnt
-        res, cnt = PCAnalysis(data, params)
-        report['pca_report'] = cnt
-        return True, report
-    elif params['process'] == 'ica':
-        res, cnt = do2dGraph(data, params)
-        report['2dgraph'] = cnt
-        res, cnt = ICAnalysis(data, params)
-        report['ica_report'] = cnt
-        return True, report
-    else:
-        return False, None
-
-def SEGYreader(file_str, params):
-    if params['process']:
-        if params['process'] == 'nmo':
-            res, report = NMOfilter(filter_str, params)
-            return res, report
-        elif params['process'] == 'ffilter':
-            res, report = FFilter(file_str, params)
-            return res, report
-        else:
-            print('nothing to process')
-            return False, None
-
-def DATreader(file_str, params):
-    pass
-def ZIPreader(file_str, params):
-    unzip(file_str)
-    return True 
-    
-def TARreader(fileobject):
-    import tarfile
-    with tarfile.open(fileobject, 'r:*') as tar_ref:
-        tar_ref.extractall(extract_to)
-
-def isvalid_base64(s):
-    try:
-        base64.b64decode(s, validate=True)
-        return True
-    except Exception:
-        return False
-
-def ValidateFile(file_str):
-    if file_str == None:
-        return False, 'None file'
-    elif file_str == '':
-        return False, 'empty string'
-    elif not isinstance(file_str, str):
-        return False, 'wrong file type'
-    elif isinstance(file_str, str):
-        base64_file = file_str.encode('utf-8')
-        if isvalid_base64(base64_file):
-            return True, 'valid base64 file'
-        else:
-            return False,'the file is not a valid base64'
-    else:
-        return False, 'something went wrong'
-
-def FileReader(file_str, params):
-    res, msg = ValidateFile(file_str) #is or not a file
-    if res == False:
-        return False, msg
-
-    if FileType(file_str, 'csv'):
-        res, report = CSVreader(file_str, params)
-        if res:
-            return True, report
-        else:
-            return False, None
-    elif FileType(file_str, 'dat'):
-        res, report = DATreader(file_str, params), True
-        return True, report
-    elif FileType(file_str, 'segy'):
-        res, report = SEGYreader(file_str, params)
-        if res:
-            return True, report
-        else:
-            return False, None 
-    elif FileType(file_str, 'zip'):
-        res, report = ZIPread(file_str)
-        return True, report
-    elif FileType(file_str, 'tar'):
-        res, report = TARreader(file_str)
-        return True, report
-    else:
-        return False, 'Filetype not recognized' 
-
--- link/api/views.py
+++ link/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework import permissions, authentication, status
+from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
 
 from django.contrib import messages
 from django.shortcuts import redirect
@@ -10,95 +12,57 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 
 import datetime
-from link.api.fs import FileReader
+import ast
+import json
+from link.api.fs import FileReader, File
 
 class CreateDataSet(APIView):
-    def post(self, request):
-        data        = request.data
-        username    = data.get('username')
-        email       = data.get('email')
-        filedata    = data.get('file')
-        filekind    = data.get('filekind')
-        process     = ''
-        params      = {}
-        if (filekind == 'csv'):
-            pca         = data.get('pca')
-            ica         = data.get('ica')
-            complete    = data.get('complete')
-            basics      = data.get('basics')
-            ydata       = data.get('ydata')
-            xdata       = data.get('xdata')
-            params['ydata'] = ydata
-            params['xdata'] = xdata
-            if(pca == 1):
-                process = 'pca'
-                params['target'] = data.get('target')
-                params['ncomps'] = data.get('ncomps')
-                params['process'] = 'pca'
-            elif (ica == 1):
-                process = 'ica'
-                params['target'] = data.get('target')
-                params['ncomps'] = data.get('ncomps')
-                params['process'] = 'ica'
-            elif (complete == 1):
-                process = 'complete'
-                params['target'] = data.get('target')
-                params['ncomps'] = data.get('ncomps')
-                params['process'] = 'complete'
-            elif (basics == 1):
-                process = 'basics'
-                params['target']    = data.get('target')
-                params['process']   = 'basics'
-            else:
-                process = ''
-        elif (filekind == 'segy'):
-            nmo = data.get('nmo')
-            ffilter = data.get('ffilter')
-            convolv = data.get('convolve')
-            if(nmo == 1):
-                process = 'nmo'
-            elif (ffilter == 1):
-                process = 'ffilter'
-                params['gain']      = data.get('gain')
-                params['fs']        = data.get('fs')
-                params['cut_freq']  = data.get('cut_freq')
-                params['filtertype'] = data.get('filtertype')
-                params['filter_name'] = data.get('filter_name')
-                params['window']    = data.get('window')
-                params['numtaps']   = data.get('numtaps')
-                params['order']     = data.get('order')
-            elif (convolv == 1):
-                process = 'convolve'
-            else:
-                process = None
-            params['process'] = process
+    parser_classes = (MultiPartParser, FormParser)
+    
+    def post(self, request, *args, **kwargs):
+        if not ("metadata" in request.data and "file" in request.FILES):
+            response = {
+                'status':status.HTTP_400_BAD_REQUEST,
+            }
+            return Response(response)
+        meta = request.data.get('metadata')
+        data = json.loads(meta)
+        file = request.FILES.get('file')
 
         MSG = "hello " + username
         response = {
                 'msg':MSG,
                 'data':None,
                 'status':status.HTTP_200_OK,
-                'process':process,
                 'filekind':filekind
         }
-        worked, report = FileReader(filedata, params)
-        if worked:
-            response['report'] = report
+        FILE = File(file, data)
+        FILE.read_file()
+        if FILE.report != {}:
+            response['report'] = FILE.report
+            response['msg'] = f"File processed"
         else:
             response['msg'] = f"File cannot be process"
+            response['status'] = status.HTTPS_500_INTERNAL_SERVER_ERROR
         return Response(response)
+
     def get(self, request):
-        message = {
-                'error':'not allowed method GET'
+        response = {
+            'status':status.HTTP_405_METHOD_NOT_ALLOWED,
         }
-        return Response(message)
+        return Response(response)
 
 class DeleteDataSet(APIView):
     def post(self, request):
-        return HttpResponse("not found get method for the link", status=404)
+        response = {
+            'status':status.HTTP_405_METHOD_NOT_ALLOWED,
+        }
+        return Response(response)
     def get(self, request):
-        return HttpResponse("deleting", status=200)
-
+        response = {
+            'status':status.HTTP_200_OK,
+        }
+        return Response(response)
 
 class AddRecord(APIView):
     def post(self, request):
